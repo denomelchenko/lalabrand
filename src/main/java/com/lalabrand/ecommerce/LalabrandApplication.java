7032109d04b3789f8fb67b9a45ae843e73d283f0
Denys <denysomelchenko.dev@gmail.com>
2024-02-08 12:40:46 +0100

    Initial commit

---
+++ src/main/java/com/lalabrand/ecommerce/LalabrandApplication.java
@@ -0,0 +1,13 @@
+package com.lalabrand.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LalabrandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LalabrandApplication.class, args);
+	}
+
+}
