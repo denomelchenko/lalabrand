19dc4c18cfe168b3d5f57fab125d091a10d34e00
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-02-19 14:09:17 +0100

    Add all models for order

---
+++ src/main/java/com/lalabrand/ecommerce/order/ordered_item/OrderedItem.java
@@ -0,0 +1,51 @@
+package com.lalabrand.ecommerce.order.ordered_item;
+
+import com.lalabrand.ecommerce.item.Item;
+import com.lalabrand.ecommerce.order.Order;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "ordered_item")
+public class OrderedItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "size", nullable = false)
+    private String size;
+
+    @Column(name = "color", nullable = false)
+    private String color;
+
+    @Column(name = "price", nullable = false, precision = 10)
+    private BigDecimal price;
+
+    @Column(name = "count", nullable = false)
+    private Integer count;
+
+    @Column(name = "image", nullable = false)
+    private String image;
+
+}
