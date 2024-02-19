5a98ef1fa6002a1406e833d8411fd894562f4c66
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-02-19 14:08:57 +0100

    Add all models for item

---
+++ src/main/java/com/lalabrand/ecommerce/item/item_comment/ItemComment.java
@@ -0,0 +1,44 @@
+package com.lalabrand.ecommerce.item.item_comment;
+
+import com.lalabrand.ecommerce.user.User;
+import com.lalabrand.ecommerce.item.Item;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "item_comment")
+public class ItemComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @Lob
+    @Column(name = "text", nullable = false)
+    private String text;
+
+    @Column(name = "raiting", nullable = false, precision = 10)
+    private BigDecimal raiting;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+}
