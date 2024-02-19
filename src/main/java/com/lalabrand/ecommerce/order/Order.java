b70ae935dfcd83d6dd149c057f71e349efad3369
Denys Omelchenko <d.omelchenko@droplo.com>
2024-02-19 14:09:17 +0100

    Add all models for order

---
+++ src/main/java/com/lalabrand/ecommerce/order/Order.java
@@ -0,0 +1,37 @@
+package com.lalabrand.ecommerce.order;
+
+import com.lalabrand.ecommerce.user.User;
+import com.lalabrand.ecommerce.order.ordered_item.OrderedItem;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "`order`")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @OneToMany(mappedBy = "order")
+    private Set<OrderedItem> orderedItems = new LinkedHashSet<>();
+
+}
