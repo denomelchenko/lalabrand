01b1a5e88730ecb522f1765297f67ac12ea30183
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-04 19:50:52 +0100

    Implement enpoint for best sellers items

--- src/main/java/com/lalabrand/ecommerce/item/ItemController.java
+++ src/main/java/com/lalabrand/ecommerce/item/ItemController.java
@@ -1,7 +1,23 @@
 package com.lalabrand.ecommerce.item;
 
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class ItemController {
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @QueryMapping
+    public List<ItemDto> getBestSellers(@Argument Integer limit) {
+        return itemService.getBestSellersItems(5);
+    }
 }
