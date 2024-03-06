e7c4495643df3d80df366d06cefd4f5f94fa2c16
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-06 14:01:12 +0100

    Add look entity

---
+++ src/main/java/com/lalabrand/ecommerce/item/look/Look.java
@@ -0,0 +1,38 @@
+package com.lalabrand.ecommerce.item.look;
+
+import com.lalabrand.ecommerce.item.Item;
+import com.lalabrand.ecommerce.item.enums.Gender;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "look")
+public class Look {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "gender")
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(name = "image")
+    private String image;
+
+    @ManyToMany
+    @JoinTable(name = "look_item",
+            joinColumns = @JoinColumn(name = "look_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    private Set<Item> items = new LinkedHashSet<>();
+
+}
