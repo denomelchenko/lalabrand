2401cd3a86406a16cb2123f39b530d81f719b542
denomelchenko <denysomelchenko.dev@gmail.com>
2024-03-07 21:19:30 +0200

    finalization category endpoint

--- src/main/java/com/lalabrand/ecommerce/item/ItemController.java
+++ src/main/java/com/lalabrand/ecommerce/item/ItemController.java
@@ -6,33 +6,29 @@ import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
-
 import java.util.Optional;
-
 
 @Controller
 public class ItemController {
     private final ItemService itemService;
-    private final ItemRepository itemRepository;
 
-    public ItemController(ItemService itemService, ItemRepository itemRepository) {
+    public ItemController(ItemService itemService) {
         this.itemService = itemService;
-        this.itemRepository = itemRepository;
     }
 
-    @QueryMapping
-    public List<Item> getItemsByCategoryId(@Argument Integer id) {
-        return itemRepository.findAllByCategoryId(id);
+    @QueryMapping(name = "itemsByCategoryId")
+    public List<ItemDto> getItemsByCategoryId(@Argument Integer categoryId) {
+        return itemService.findItemsByCategoryId(categoryId);
+    }
 
-        @QueryMapping(name = "bestSellers")
-        public List<ItemDto> findBestSellers (@Argument Optional <Integer> limit) {
-            return itemService.findBestSellersItems(limit);
-        }
+    @QueryMapping(name = "bestSellers")
+    public List<ItemDto> findBestSellers(@Argument Optional<Integer> limit) {
+        return itemService.findBestSellersItems(limit);
+    }
 
-        @QueryMapping(name = "itemsByTitle")
-        public List<ItemDto> findItemsByTitle (@Argument String title){
-            return itemService.findItemsByTitle(title);
-
-        }
+    @QueryMapping(name = "itemsByTitle")
+    public List<ItemDto> findItemsByTitle(@Argument String title) {
+        return itemService.findItemsByTitle(title);
     }
 }
+
