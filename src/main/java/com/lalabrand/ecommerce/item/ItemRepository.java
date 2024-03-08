82dc5cc790469f78486237021279f1a456cd2b5a
denomelchenko <denysomelchenko.dev@gmail.com>
2024-03-08 23:23:39 +0200

    category-endpoint-fix
    I made a CategoryService and changed the CategoryController, also in the ItemController I changed the name get to find and removed the empty lines in the ItemRepository

--- src/main/java/com/lalabrand/ecommerce/item/ItemRepository.java
+++ src/main/java/com/lalabrand/ecommerce/item/ItemRepository.java
@@ -6,12 +6,9 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Integer> {
     List<Item> findAllByCategoryId(Integer categoryId);
     List<Item> findItemsByOrderBySoldCountDesc(Pageable pageable);
-
     List<Item> findByTitleContainingIgnoreCase(String title);
-
 }
