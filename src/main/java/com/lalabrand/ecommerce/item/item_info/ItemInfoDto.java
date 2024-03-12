c182f25e028bb64545dd9570a22c104b44bf9356
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-12 17:10:45 +0100

    Implement item info dto

---
+++ src/main/java/com/lalabrand/ecommerce/item/item_info/ItemInfoDto.java
@@ -0,0 +1,29 @@
+package com.lalabrand.ecommerce.item.item_info;
+
+import com.lalabrand.ecommerce.item.ItemDto;
+import com.lalabrand.ecommerce.item.enums.ColorEnum;
+import lombok.Builder;
+import lombok.Value;
+
+import java.io.Serializable;
+
+/**
+ * DTO for {@link ItemInfo}
+ */
+@Value
+@Builder
+public class ItemInfoDto implements Serializable {
+    Integer id;
+    ItemDto item;
+    ColorEnum color;
+    String image;
+
+    public static ItemInfoDto fromEntity(ItemInfo itemInfo) {
+        return ItemInfoDto.builder()
+                .id(itemInfo.getId())
+                .item(ItemDto.fromEntity(itemInfo.getItem()))
+                .color(itemInfo.getColor())
+                .image(itemInfo.getImage())
+                .build();
+    }
+}
