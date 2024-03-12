b7f5283a876113df752d07c49d5be8a35f8fc901
Alexey <oleksijku20@gmail.com>
2024-03-12 16:18:38 +0200

    category-endpoind-testCategoryService
    I did a test for service categories

---
+++ src/test/java/com/lalabrand/ecommerce/item/Category/CategoryServiceTest.java
@@ -0,0 +1,35 @@
+package com.lalabrand.ecommerce.item.Category;
+
+import com.lalabrand.ecommerce.item.category.Category;
+import com.lalabrand.ecommerce.item.category.CategoryDTO;
+import com.lalabrand.ecommerce.item.category.CategoryRepository;
+import com.lalabrand.ecommerce.item.category.CategoryService;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CategoryServiceTest {
+
+    @Test
+    public void test_getAllCategory(){
+
+        CategoryRepository categoryRepository = mock(CategoryRepository.class);
+        CategoryService categoryService = new CategoryService(categoryRepository);
+
+        List<Category> categories = new ArrayList<>();
+        categories.add(new Category(1, "Category 1", new LinkedHashSet<>()));
+        categories.add(new Category(2, "Category 2", new LinkedHashSet<>()));
+
+        when(categoryRepository.findAll()).thenReturn(categories);
+
+        List<CategoryDTO> result = categoryService.findAllCategory();
+
+        assertEquals(2, result.size());
+    }
+}
