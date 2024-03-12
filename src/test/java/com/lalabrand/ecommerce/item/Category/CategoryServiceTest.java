798c471a9403ab11b90847f299da9fe4225a4a66
Alexey <oleksijku20@gmail.com>
2024-03-12 17:18:01 +0200

    category-endpoint-test update
    I updated the test for service categories, where I added the initialization of CategoryRepository and CategoryService using BeforEach

--- src/test/java/com/lalabrand/ecommerce/item/Category/CategoryServiceTest.java
+++ src/test/java/com/lalabrand/ecommerce/item/Category/CategoryServiceTest.java
@@ -4,6 +4,8 @@ import com.lalabrand.ecommerce.item.category.Category;
 import com.lalabrand.ecommerce.item.category.CategoryDTO;
 import com.lalabrand.ecommerce.item.category.CategoryRepository;
 import com.lalabrand.ecommerce.item.category.CategoryService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -15,13 +17,16 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class CategoryServiceTest {
+    private  CategoryRepository categoryRepository;
+    private  CategoryService categoryService;
 
+    @BeforeEach
+    public void initializationCategoryRepositoryAndCategoryService(){
+        categoryRepository = mock(CategoryRepository.class);
+        categoryService = new CategoryService(categoryRepository);
+    }
     @Test
     public void test_getAllCategory(){
-
-        CategoryRepository categoryRepository = mock(CategoryRepository.class);
-        CategoryService categoryService = new CategoryService(categoryRepository);
-
         List<Category> categories = new ArrayList<>();
         categories.add(new Category(1, "Category 1", new LinkedHashSet<>()));
         categories.add(new Category(2, "Category 2", new LinkedHashSet<>()));
