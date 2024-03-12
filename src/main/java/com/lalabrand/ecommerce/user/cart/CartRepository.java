3fb8ebf8618d3f14e6fca9a388ae42318c92a009
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-12 12:31:10 +0100

    Add CartRepository

---
+++ src/main/java/com/lalabrand/ecommerce/user/cart/CartRepository.java
@@ -0,0 +1,8 @@
+package com.lalabrand.ecommerce.user.cart;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Integer> {
+}
