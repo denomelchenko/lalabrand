56133e02667c65445b0fdcf178d6d98009783f73
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-12 13:37:07 +0100

    Update cart repository, add find cart by user id

--- src/main/java/com/lalabrand/ecommerce/user/cart/CartRepository.java
+++ src/main/java/com/lalabrand/ecommerce/user/cart/CartRepository.java
@@ -1,8 +1,12 @@
 package com.lalabrand.ecommerce.user.cart;
 
+import com.lalabrand.ecommerce.item.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Integer> {
+    Cart findCartByUserId(Integer userId);
 }
