2d3d82479dcb2169c57012bd4c18271ad5b21e1e
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-12 18:29:10 +0100

    Cart service updates

--- src/main/java/com/lalabrand/ecommerce/user/cart/CartController.java
+++ src/main/java/com/lalabrand/ecommerce/user/cart/CartController.java
@@ -5,6 +5,8 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import java.util.Optional;
+
 @Controller
 public class CartController {
     private final CartService cartService;
