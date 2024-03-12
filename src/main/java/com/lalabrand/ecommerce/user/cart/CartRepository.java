b1370ea35dd909cbecc3f6df903a78906e3289e0
Denys Omelchenko <d.omelchenko@droplo.com>
2024-03-12 18:29:10 +0100

    Cart service updates

--- src/main/java/com/lalabrand/ecommerce/user/cart/CartRepository.java
+++ src/main/java/com/lalabrand/ecommerce/user/cart/CartRepository.java
@@ -3,7 +3,9 @@ package com.lalabrand.ecommerce.user.cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Integer> {
-    Cart findCartByUserId(Integer userId);
+    Optional<Cart> findCartByUserId(Integer userId);
 }
--- src/main/java/com/lalabrand/ecommerce/user/cart/CartService.java
+++ src/main/java/com/lalabrand/ecommerce/user/cart/CartService.java
@@ -1,22 +1,28 @@
 package com.lalabrand.ecommerce.user.cart;
 
-import com.lalabrand.ecommerce.item.Item;
-import com.lalabrand.ecommerce.item.ItemDto;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CartService {
     private final CartRepository cartRepository;
 
     @Autowired
-    public CartService(CartRepository cartRepository) {
+    public CartService(final CartRepository cartRepository) {
         this.cartRepository = cartRepository;
     }
 
-    public CartDto findCartItemsByUserId(Integer userId) {
-        return CartDto.fromEntity(cartRepository.findCartByUserId(userId));
+    public CartDto findCartItemsByUserId(final Integer userId) {
+        if (userId == null) {
+            throw new IllegalArgumentException("userId cannot be null");
+        }
+        Optional<Cart> cart = cartRepository.findCartByUserId(userId);
+        if (cart.isEmpty()) {
+            throw new EntityNotFoundException("Cart for this user is`t exist");
+        }
+        return CartDto.fromEntity(cart.get());
     }
 }
