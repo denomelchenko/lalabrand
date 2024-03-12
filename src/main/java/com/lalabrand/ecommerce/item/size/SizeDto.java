c7d15ae2386f7e7cb4e627fcdcfdf186054997ff
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-12 17:10:10 +0100

    Implement size dto

---
+++ src/main/java/com/lalabrand/ecommerce/item/size/SizeDto.java
@@ -0,0 +1,21 @@
+package com.lalabrand.ecommerce.item.size;
+
+import com.lalabrand.ecommerce.item.enums.SizeType;
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+public class SizeDto {
+    Integer id;
+    SizeType sizeType;
+    String value;
+
+    public static SizeDto fromEntity(Size size) {
+        return SizeDto.builder()
+                .id(size.getId())
+                .sizeType(size.getSizeType())
+                .value(size.getValue())
+                .build();
+    }
+}
