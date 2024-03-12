3bfbff74f971943f6c30a98ad6a8f426af68a325
Denys Omelchenko <d.omelchenko@droplo.com>
2024-03-12 13:51:58 +0100

    Add item info entity + cart item entity

---
+++ src/main/java/com/lalabrand/ecommerce/user/cart/cart_item/CartItem.java
@@ -0,0 +1,40 @@
+package com.lalabrand.ecommerce.user.cart.cart_item;
+
+import com.lalabrand.ecommerce.item.Item;
+import com.lalabrand.ecommerce.item.item_info.ItemInfo;
+import com.lalabrand.ecommerce.item.size.Size;
+import com.lalabrand.ecommerce.user.cart.Cart;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "cart_item")
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_info_id")
+    private ItemInfo itemInfo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "size_id")
+    private Size size;
+
+    @Column(name = "count")
+    private Integer count;
+
+}
