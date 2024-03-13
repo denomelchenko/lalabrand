aaf825e2f2f9af8733d1ff5934272876d2517c17
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-13 15:10:13 +0100

    Fix + add logger

--- src/main/java/com/lalabrand/ecommerce/user/cart/CartService.java
+++ src/main/java/com/lalabrand/ecommerce/user/cart/CartService.java
@@ -1,6 +1,5 @@
 package com.lalabrand.ecommerce.user.cart;
 
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
