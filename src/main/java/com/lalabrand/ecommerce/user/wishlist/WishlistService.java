04c36216eb52a7bf28413b09029225c8d6614d06
Denys Omelchenko <d.omelchenko@droplo.com>
2024-03-13 15:10:13 +0100

    Fix + add logger

--- src/main/java/com/lalabrand/ecommerce/user/wishlist/WishlistService.java
+++ src/main/java/com/lalabrand/ecommerce/user/wishlist/WishlistService.java
@@ -1,5 +1,7 @@
 package com.lalabrand.ecommerce.user.wishlist;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,7 @@ import java.util.Optional;
 @Service
 public class WishlistService {
     private final WishlistRepository wishlistRepository;
+    private final Logger logger = LoggerFactory.getLogger(WishlistService.class);
 
     @Autowired
     public WishlistService(WishlistRepository wishlistRepository) {
@@ -15,6 +18,18 @@ public class WishlistService {
     }
 
     public Optional<WishlistDto> findWishlistByUserId(Integer userId) {
-        return wishlistRepository.findWishlistByUserId(userId).map(WishlistDto::fromEntity);
+        try {
+            Optional<Wishlist> wishlist = wishlistRepository.findWishlistByUserId(userId);
+            if (wishlist.isPresent()) {
+                logger.debug("Wishlist found for user with ID: {}", userId);
+                return wishlist.map(WishlistDto::fromEntity);
+            } else {
+                logger.debug("No wishlist found for user with ID: {}", userId);
+                return Optional.empty();
+            }
+        } catch (Exception e) {
+            logger.error("Failed to find wishlist for user with ID: {}", userId, e);
+            throw new RuntimeException("Failed to find wishlist for user with ID: " + userId, e);
+        }
     }
 }
