8bf8f839bfd5c111f83f64a92c722c45f00b7f94
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-17 13:09:12 +0100

    Update category controller

--- src/main/java/com/lalabrand/ecommerce/item/category/CategoryController.java
+++ src/main/java/com/lalabrand/ecommerce/item/category/CategoryController.java
@@ -1,16 +1,26 @@
 package com.lalabrand.ecommerce.item.category;
 
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+
 import java.util.List;
+
 @Controller
 public class CategoryController {
     private final CategoryService categoryService;
+
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
+
     @QueryMapping(name = "allCategories")
-    public List<CategoryDTO> allCategories(){
+    public List<CategoryDTO> findAllCategories() {
         return categoryService.findAllCategory();
+    }
+
+    @QueryMapping(name = "categoryById")
+    public CategoryDTO findCategoryById(@Argument Integer id) {
+        return categoryService.findCategoryById(id).orElse(null);
     }
 }
