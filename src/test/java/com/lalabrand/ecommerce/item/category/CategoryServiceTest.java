5e01bcca9cfeba81bf21187d737622700cb648fe
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-17 11:44:52 +0100

    Refactor for category test

--- src/test/java/com/lalabrand/ecommerce/item/category/CategoryServiceTest.java
+++ src/test/java/com/lalabrand/ecommerce/item/category/CategoryServiceTest.java
@@ -1,11 +1,6 @@
-package com.lalabrand.ecommerce.item.Category;
+package com.lalabrand.ecommerce.item.category;
 
-import com.lalabrand.ecommerce.item.category.Category;
-import com.lalabrand.ecommerce.item.category.CategoryDTO;
-import com.lalabrand.ecommerce.item.category.CategoryRepository;
-import com.lalabrand.ecommerce.item.category.CategoryService;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -17,11 +12,11 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class CategoryServiceTest {
-    private  CategoryRepository categoryRepository;
-    private  CategoryService categoryService;
+    private static CategoryRepository categoryRepository;
+    private static CategoryService categoryService;
 
-    @BeforeEach
-    public void initializationCategoryRepositoryAndCategoryService(){
+    @BeforeAll
+    public static void initializationCategoryRepositoryAndCategoryService(){
         categoryRepository = mock(CategoryRepository.class);
         categoryService = new CategoryService(categoryRepository);
     }
