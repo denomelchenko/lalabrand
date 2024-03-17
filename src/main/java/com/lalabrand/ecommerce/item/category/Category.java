0c908f04b61980a9262379cba2a01c82daca6bde
Denys Omelchenko <d.omelchenko@droplo.com>
2024-03-17 12:59:11 +0100

    Code refactor for category entity

--- src/main/java/com/lalabrand/ecommerce/item/category/Category.java
+++ src/main/java/com/lalabrand/ecommerce/item/category/Category.java
@@ -2,10 +2,7 @@ package com.lalabrand.ecommerce.item.category;
 
 import com.lalabrand.ecommerce.item.Item;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Set;
 
