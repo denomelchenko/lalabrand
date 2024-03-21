b8aa73a62e6565b7246af120cfb8691d9fcc12be
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-21 10:11:17 +0100

    Implement throwing errors for services

--- src/main/java/com/lalabrand/ecommerce/item/category/CategoryService.java
+++ src/main/java/com/lalabrand/ecommerce/item/category/CategoryService.java
@@ -1,15 +1,21 @@
 package com.lalabrand.ecommerce.item.category;
 
+import com.lalabrand.ecommerce.user.wishlist.WishlistService;
+import jakarta.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @Service
 public class CategoryService {
     private final CategoryRepository categoryRepository;
+    private final Logger logger = LoggerFactory.getLogger(CategoryService.class);
 
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
@@ -17,20 +23,30 @@ public class CategoryService {
 
     public List<CategoryDTO> findAllCategory() {
         try {
-            return categoryRepository.findAll().stream().map(CategoryDTO::fromEntity).toList();
-        } catch (Exception e) {
-            return Collections.emptyList();
+            List<Category> categories = categoryRepository.findAll();
+            if (categories.isEmpty()) {
+                throw new EntityNotFoundException("No categories found");
+            }
+            return categories.stream()
+                    .map(CategoryDTO::fromEntity)
+                    .collect(Collectors.toList());
+        } catch (EntityNotFoundException ex) {
+            logger.info("No categories found: " + ex.getMessage());
+            throw ex;
+        } catch (Exception ex) {
+            logger.info("Error occurred while fetching categories: " + ex.getMessage());
+            throw new RuntimeException("Error occurred while fetching categories", ex);
         }
     }
 
     public Optional<CategoryDTO> findCategoryById(Integer id) {
-        try {
-            if (id == null || id < 1) {
-                throw new IllegalArgumentException("Id can not be null or less than 1");
-            }
-            return categoryRepository.findById(id).map(CategoryDTO::fromEntity);
-        } catch (IllegalArgumentException illegalArgumentException) {
-            return Optional.empty();
+        if (id == null || id < 1) {
+            throw new IllegalArgumentException("Id can not be null or less than 1");
         }
+        Optional<Category> categoryEntityOptional = categoryRepository.findById(id);
+        if (categoryEntityOptional.isEmpty()) {
+            throw new EntityNotFoundException("Category not found for id: " + id);
+        }
+        return categoryEntityOptional.map(CategoryDTO::fromEntity);
     }
 }
