064c745979b0e78da379327931ec7eba6b25b0e2
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-27 16:54:31 +0200

    Update user role

--- src/main/java/com/lalabrand/ecommerce/user/role/UserRole.java
+++ src/main/java/com/lalabrand/ecommerce/user/role/UserRole.java
@@ -3,17 +3,18 @@ package com.lalabrand.ecommerce.user.role;
 import com.lalabrand.ecommerce.user.User;
 import com.lalabrand.ecommerce.user.enums.Role;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "user_roles")
 public class UserRole {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Integer id;
 
@@ -21,9 +22,13 @@ public class UserRole {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "user_id")
     private User user;
 
+    public UserRole(Role role, User user) {
+        this.role = role;
+        this.user = user;
+    }
 }
