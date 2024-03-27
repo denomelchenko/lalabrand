7cf44c2772e90800c7270933216918619098332f
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-27 13:24:49 +0200

    Add security to CartController

--- src/main/java/com/lalabrand/ecommerce/user/cart/CartController.java
+++ src/main/java/com/lalabrand/ecommerce/user/cart/CartController.java
@@ -3,9 +3,11 @@ package com.lalabrand.ecommerce.user.cart;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 
-import java.util.Optional;
+import java.security.Security;
 
 @Controller
 public class CartController {
@@ -17,8 +19,8 @@ public class CartController {
     }
 
     @QueryMapping(name = "cartByUserId")
-    public CartDto findCartByUserId(@Argument Integer userId) {
+    @PreAuthorize("hasAuthority('USER')")
+    public CartDTO findCartByUserId(@Argument Integer userId) {
         return cartService.findCartByUserId(userId).orElse(null);
-        //TODO: Add security here
     }
 }
