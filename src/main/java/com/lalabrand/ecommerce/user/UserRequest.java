fe0b8ffcd4960aa201639d9a3f4c89ec46c43211
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-27 21:21:41 +0200

    Add password validation annotation

--- src/main/java/com/lalabrand/ecommerce/user/UserRequest.java
+++ src/main/java/com/lalabrand/ecommerce/user/UserRequest.java
@@ -1,5 +1,26 @@
 package com.lalabrand.ecommerce.user;
 
-public record UserRequest(String password, String email, Integer id) {
+import com.lalabrand.ecommerce.utils.Password;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class UserRequest {
+    @NotBlank(message = "Password can not be blank")
+    @Password
+    String password;
+
+    @NotBlank(message = "Email can not be blank")
+    @Email(message = "Invalid email address")
+    String email;
+
+    Integer id;
 }
 
