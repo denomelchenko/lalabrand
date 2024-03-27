b9cb14073154e4078b5d371a7603aa884b8f5747
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-27 18:08:49 +0200

    Updates

---
+++ src/main/java/com/lalabrand/ecommerce/utils/UserAccessChecker.java
@@ -0,0 +1,27 @@
+package com.lalabrand.ecommerce.utils;
+
+import com.lalabrand.ecommerce.user.User;
+import com.lalabrand.ecommerce.user.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class UserAccessChecker {
+    private final UserService userService;
+
+    public UserAccessChecker(UserService userService) {
+        this.userService = userService;
+    }
+
+    public boolean isCurrentUserEqualsId(Integer id) {
+        Optional<User> user = userService.findByUserId(id);
+        if (user.isPresent()) {
+            System.out.println(user.get().getEmail());
+            System.out.println(SecurityContextHolder.getContext().getAuthentication().getName());
+            return user.get().getEmail().equals(SecurityContextHolder.getContext().getAuthentication().getName());
+        }
+        return false;
+    }
+}
