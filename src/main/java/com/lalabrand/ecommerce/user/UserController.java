1b3bbb9f85aa9f38e412ae142e07f6e5b18e8b51
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-27 13:21:56 +0200

    Update for UserController

--- src/main/java/com/lalabrand/ecommerce/user/UserController.java
+++ src/main/java/com/lalabrand/ecommerce/user/UserController.java
@@ -1,7 +1,63 @@
 package com.lalabrand.ecommerce.user;
 
+import com.lalabrand.ecommerce.auth.*;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserController {
+    private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final RefreshTokenService refreshTokenService;
+    private final JwtService jwtService;
+
+    public UserController(UserService userService, AuthenticationManager authenticationManager, RefreshTokenService refreshTokenService, JwtService jwtService) {
+        this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.refreshTokenService = refreshTokenService;
+        this.jwtService = jwtService;
+    }
+
+    @MutationMapping(name = "user")
+    public UserResponse saveUser(@Argument UserRequest userRequest) {
+        try {
+            return userService.saveUser(userRequest);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @MutationMapping(name = "login")
+    public JwtResponseDTO loginUserAndGetTokens(@Argument AuthRequestDTO authRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword())
+        );
+        if (authentication.isAuthenticated()) {
+            RefreshToken refreshToken = refreshTokenService.createRefreshToken(authRequest.getEmail());
+            return JwtResponseDTO.builder()
+                    .token(refreshToken.getToken())
+                    .accessToken(jwtService.generateToken(authRequest.getEmail()))
+                    .build();
+        } else {
+            throw new UsernameNotFoundException("Invalid request");
+        }
+    }
+
+    @MutationMapping(name = "refreshToken")
+    public JwtResponseDTO refreshAccessToken(@Argument RefreshTokenRequestDTO refreshTokenRequest) {
+        return refreshTokenService.findByToken(refreshTokenRequest.getToken())
+                .map(refreshTokenService::verifyExpiration)
+                .map(RefreshToken::getUser)
+                .map(user -> {
+                    String accessToken = jwtService.generateToken(user.getEmail());
+                    return JwtResponseDTO.builder()
+                            .accessToken(accessToken)
+                            .token(refreshTokenRequest.getToken()).build();
+                }).orElseThrow(() -> new RuntimeException("Refresh token is not valid"));
+    }
 }
