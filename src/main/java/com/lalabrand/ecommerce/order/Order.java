040ef938a0f18fa887acadc9a3df0a0705e3317b
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-27 16:56:45 +0200

    Update order entity

--- src/main/java/com/lalabrand/ecommerce/order/Order.java
+++ src/main/java/com/lalabrand/ecommerce/order/Order.java
@@ -60,4 +60,11 @@ public class Order {
     @JoinColumn(name = "shipping_id")
     private ShippingInfo shipping;
 
+    @PrePersist
+    public void prePersist() {
+        if (createdAt == null) {
+            createdAt = Instant.now();
+        }
+    }
+
 }
