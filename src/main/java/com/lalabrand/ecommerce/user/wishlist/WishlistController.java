cbdeeb91374c74521dff962fef355c92d32ec489
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-27 13:23:54 +0200

    Add security to WishlistController

--- src/main/java/com/lalabrand/ecommerce/user/wishlist/WishlistController.java
+++ src/main/java/com/lalabrand/ecommerce/user/wishlist/WishlistController.java
@@ -3,6 +3,7 @@ package com.lalabrand.ecommerce.user.wishlist;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -15,7 +16,8 @@ public class WishlistController {
     }
 
     @QueryMapping(name = "wishlistByUserId")
-    public WishlistDto findWishlistByUserId(@Argument Integer userId) {
+    @PreAuthorize("hasAuthority('USER')")
+    public WishlistDTO findWishlistByUserId(@Argument Integer userId) {
         return wishlistService.findWishlistByUserId(userId).orElse(null);
     }
 }
