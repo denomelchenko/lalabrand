d0694d3442c781c9e820bf6538658b6f54890dba
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-27 13:17:51 +0200

    Update User entity

--- src/main/java/com/lalabrand/ecommerce/user/User.java
+++ src/main/java/com/lalabrand/ecommerce/user/User.java
@@ -1,11 +1,11 @@
 package com.lalabrand.ecommerce.user;
 
-import com.lalabrand.ecommerce.user.enums.Language;
-import com.lalabrand.ecommerce.user.role.UserRole;
 import com.lalabrand.ecommerce.item.item_comment.ItemComment;
 import com.lalabrand.ecommerce.order.Order;
 import com.lalabrand.ecommerce.user.address.Address;
 import com.lalabrand.ecommerce.user.cart.Cart;
+import com.lalabrand.ecommerce.user.enums.Language;
+import com.lalabrand.ecommerce.user.role.UserRole;
 import com.lalabrand.ecommerce.user.wishlist.Wishlist;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -13,6 +13,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.Clock;
 import java.time.Instant;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -29,13 +30,10 @@ public class User {
     @Column(name = "id", nullable = false)
     private Integer id;
 
-    @Column(name = "username", nullable = false, length = 50)
-    private String username;
-
-    @Column(name = "first_name", nullable = false, length = 50)
+    @Column(name = "first_name", length = 50)
     private String firstName;
 
-    @Column(name = "last_name", nullable = false, length = 50)
+    @Column(name = "last_name", length = 50)
     private String lastName;
 
     @Column(name = "email", nullable = false, length = 50)
@@ -68,11 +66,24 @@ public class User {
     @Column(name = "password")
     private String password;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
     private Set<UserRole> userRoles = new LinkedHashSet<>();
 
     @Column(name = "language")
     @Enumerated(EnumType.STRING)
     private Language language;
 
+    public User(Integer id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        if (createdAt == null) {
+            createdAt = Instant.now();
+            System.out.println(Instant.now());
+        }
+    }
 }
