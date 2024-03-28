7c404d0a00fd596e501b5888232b6dab52d6b486
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-28 14:17:11 +0200

    Fixes + code refactor

--- src/main/java/com/lalabrand/ecommerce/auth/JwtService.java
+++ src/main/java/com/lalabrand/ecommerce/auth/JwtService.java
@@ -1,6 +1,7 @@
 package com.lalabrand.ecommerce.auth;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -23,30 +24,12 @@ public class JwtService {
     @Value("${access.token.expiration.seconds}")
     private Long accessTokenExpirationSec;
 
-    public boolean validateToken(String token, UserDetails userDetails) {
-        String username = extractName(token);
-        return (username.equals(userDetails.getUsername()) && !isNotExpired(token));
+    public boolean validateToken(JwtToken jwtToken, UserDetails userDetails) {
+        return (!isNotExpired(jwtToken) && jwtToken.getSubject().equals(userDetails.getUsername()));
     }
 
-    private boolean isNotExpired(String token) {
-        return extractExpiration(token).before(new Date());
-    }
-
-    public String extractName(String token) {
-        return extractClaim(token, Claims::getSubject);
-    }
-
-    private Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
-    }
-
-    public <T> T extractClaim(String jwt, Function<Claims, T> claimsResolver) {
-        Claims claims = Jwts.parser()
-                .verifyWith(getKey())
-                .build()
-                .parseSignedClaims(jwt)
-                .getPayload();
-        return claimsResolver.apply(claims);
+    private boolean isNotExpired(JwtToken token) {
+        return token.getExpirationDate().before(new Date());
     }
 
     private SecretKey getKey() {
@@ -67,5 +50,18 @@ public class JwtService {
                 .expiration(new Date(System.currentTimeMillis() + accessTokenExpirationSec * 1000))
                 .signWith(getKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public JwtToken parseToken(String token) {
+        Claims claims = Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        String subject = claims.getSubject();
+        Date expirationDate = claims.getExpiration();
+
+        return new JwtToken(subject, expirationDate, token);
     }
 }
