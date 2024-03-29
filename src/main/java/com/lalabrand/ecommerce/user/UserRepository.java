d079012f32b823bb744dd22dbbb4eafce5769f6f
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-29 15:44:46 +0200

    Add @Repository annotation for UserRepository

--- src/main/java/com/lalabrand/ecommerce/user/UserRepository.java
+++ src/main/java/com/lalabrand/ecommerce/user/UserRepository.java
@@ -2,9 +2,11 @@ package com.lalabrand.ecommerce.user;
 
 import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
     @NonNull Optional<User> findById(@NonNull String id);
