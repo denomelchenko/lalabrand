f259e92a499a38353464882d05c58b6f126608c3
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-29 12:14:09 +0200

    Small fixes for user

--- src/main/java/com/lalabrand/ecommerce/user/UserRepository.java
+++ src/main/java/com/lalabrand/ecommerce/user/UserRepository.java
@@ -1,10 +1,11 @@
 package com.lalabrand.ecommerce.user;
 
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
-    Optional<User> findById(String id);
+    @NonNull Optional<User> findById(@NonNull String id);
 }
