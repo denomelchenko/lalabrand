94ffa608b0b2918750665c9ecf74f650da03e4b7
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-29 12:11:32 +0200

    Look repository updates

--- src/main/java/com/lalabrand/ecommerce/item/look/LookRepository.java
+++ src/main/java/com/lalabrand/ecommerce/item/look/LookRepository.java
@@ -1,12 +1,13 @@
 package com.lalabrand.ecommerce.item.look;
 
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.Instant;
 import java.util.Optional;
 
 public interface LookRepository extends JpaRepository<Look, String> {
-    Optional<Look> findFirstByOrderByCreatedAt();
-    Optional<Look> findById(String id);
-    Optional<Look> findFirstByCreatedAtAfterOrderByCreatedAt(Instant date);
+    Optional<Look> findFirstByIdGreaterThan(String id);
+    Optional<Look> findFirstByOrderByIdAsc();
+
 }
