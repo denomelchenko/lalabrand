e68246914a75158a6173590d8c4e29400aa6ebfb
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-04-01 11:33:30 +0300

    Small fix

--- src/main/java/com/lalabrand/ecommerce/utils/EmailSenderService.java
+++ src/main/java/com/lalabrand/ecommerce/utils/EmailSenderService.java
@@ -1,10 +1,8 @@
 package com.lalabrand.ecommerce.utils;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
