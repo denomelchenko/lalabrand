f8bd1bb6dfd07a2a4c4f4b9dc918548fff6f1f3c
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-04-01 19:19:39 +0300

    Add id for UserDetailsImpl

--- src/main/java/com/lalabrand/ecommerce/auth/UserDetailsImpl.java
+++ src/main/java/com/lalabrand/ecommerce/auth/UserDetailsImpl.java
@@ -2,6 +2,7 @@ package com.lalabrand.ecommerce.auth;
 
 import com.lalabrand.ecommerce.user.User;
 import com.lalabrand.ecommerce.user.role.UserRole;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,12 +13,15 @@ import java.util.List;
 
 public class UserDetailsImpl extends User implements UserDetails {
     private final String password;
-    Collection<? extends GrantedAuthority> authorities;
     private final String email;
+    @Getter
+    private final Integer id;
+    Collection<? extends GrantedAuthority> authorities;
 
     public UserDetailsImpl(User user) {
         this.email = user.getEmail();
         this.password = user.getPassword();
+        this.id = user.getId();
         List<GrantedAuthority> auths = new ArrayList<>();
 
         for (UserRole role : user.getUserRoles()) {
