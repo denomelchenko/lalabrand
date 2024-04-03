de74cab3e1c5b59044c2c5666405f2ec657a4648
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-04-03 12:34:40 +0200

    Add validation for refresh token request dto

--- src/main/java/com/lalabrand/ecommerce/security/refresh_token/RefreshTokenRequestDTO.java
+++ src/main/java/com/lalabrand/ecommerce/security/refresh_token/RefreshTokenRequestDTO.java
@@ -3,10 +3,12 @@ package com.lalabrand.ecommerce.security.refresh_token;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class RefreshTokenRequestDTO {
+    @UUID(message = "Token is not correct")
     private String token;
 }
