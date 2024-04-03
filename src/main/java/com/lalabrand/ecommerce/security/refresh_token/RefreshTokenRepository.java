a6b06e730dacc1d50a41a30dca111e83074dc85b
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-04-03 12:35:35 +0200

    Add deleteByUserId method to RefreshTokenRepository

--- src/main/java/com/lalabrand/ecommerce/security/refresh_token/RefreshTokenRepository.java
+++ src/main/java/com/lalabrand/ecommerce/security/refresh_token/RefreshTokenRepository.java
@@ -9,5 +9,7 @@ import java.util.Optional;
 @Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
     Optional<RefreshToken> findByToken(String token);
+
+    void deleteByUserId(String userId);
 }
 
