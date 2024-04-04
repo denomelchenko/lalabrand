1fe0e713623dcf0012465e510caaf42b0314bb91
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-04-04 17:08:07 +0200

    Code refactor

--- src/main/java/com/lalabrand/ecommerce/user/cart/CartController.java
+++ src/main/java/com/lalabrand/ecommerce/user/cart/CartController.java
@@ -1,17 +1,12 @@
 package com.lalabrand.ecommerce.user.cart;
 
 import com.lalabrand.ecommerce.security.UserDetailsImpl;
-import com.lalabrand.ecommerce.user.User;
 import com.lalabrand.ecommerce.utils.CommonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
-
-import java.util.Optional;
 
 @Controller
 public class CartController {
@@ -28,9 +23,9 @@ public class CartController {
     @PreAuthorize("hasAuthority('USER')")
     public CartDTO findCartForCurrentUser() {
         UserDetailsImpl user = commonUtils.getCurrentUser();
-        if (user != null) {
-            return cartService.findCartByUserId(user.getId()).orElse(null);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found!");
         }
-        throw new UsernameNotFoundException("User not found!");
+        return cartService.findCartByUserId(user.getId()).orElse(null);
     }
 }
