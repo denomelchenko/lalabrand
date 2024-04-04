c77cafe420e43492febf55a74d01b2ab4e19ca6b
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-04-04 17:08:07 +0200

    Code refactor

--- src/main/java/com/lalabrand/ecommerce/user/wishlist/WishlistController.java
+++ src/main/java/com/lalabrand/ecommerce/user/wishlist/WishlistController.java
@@ -1,15 +1,12 @@
 package com.lalabrand.ecommerce.user.wishlist;
 
 import com.lalabrand.ecommerce.security.UserDetailsImpl;
-import com.lalabrand.ecommerce.user.User;
 import com.lalabrand.ecommerce.utils.CommonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
-
-import java.util.Optional;
 
 @Controller
 public class WishlistController {
@@ -26,9 +23,10 @@ public class WishlistController {
     @PreAuthorize("hasAuthority('USER')")
     public WishlistDTO findWishlistForCurrentUser() {
         UserDetailsImpl user = commonUtils.getCurrentUser();
-        if (user != null) {
-            return wishlistService.findWishlistByUserId(user.getId()).orElse(null);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found!");
         }
-        throw new UsernameNotFoundException("User not found!");
+        return wishlistService.findWishlistByUserId(user.getId()).orElse(null);
+
     }
 }
