23c04a7a81572b80ab5cf9bd7e61670046c757ea
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-04-08 11:35:31 +0200

    Implement TokenExpiredException

---
+++ src/main/java/com/lalabrand/ecommerce/exception/TokenExpiredException.java
@@ -0,0 +1,11 @@
+package com.lalabrand.ecommerce.exception;
+
+public class TokenExpiredException extends RuntimeException {
+    public TokenExpiredException(String message) {
+        super(message);
+    }
+
+    public TokenExpiredException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
