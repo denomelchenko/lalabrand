3d14e7b8b466bba8d25c7e0562d37f4f35d594ea
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-04-09 23:25:55 +0200

    Implement addItemToWishlist method(WishlistService)

--- src/main/java/com/lalabrand/ecommerce/user/wishlist/WishlistService.java
+++ src/main/java/com/lalabrand/ecommerce/user/wishlist/WishlistService.java
@@ -1,11 +1,15 @@
 package com.lalabrand.ecommerce.user.wishlist;
 
+import com.lalabrand.ecommerce.item.Item;
+import com.lalabrand.ecommerce.user.User;
+import com.lalabrand.ecommerce.utils.CommonUtils;
 import jakarta.persistence.EntityNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.Optional;
 
 @Service
@@ -32,5 +36,20 @@ public class WishlistService {
             logger.error("Failed to get wishlist for user with ID: {}", userId, e);
             throw new EntityNotFoundException("Failed to find wishlist for user with ID: " + userId, e);
         }
+    }
+
+    public WishlistDTO addItemToWishlist(String itemId, String userId) {
+        if (CommonUtils.isIdInValid(itemId) || CommonUtils.isIdInValid(userId)) {
+            throw new IllegalArgumentException("itemId or userId is invalid");
+        }
+        Optional<Wishlist> existWishlist = wishlistRepository.findWishlistByUserId(userId);
+        if (existWishlist.isEmpty() || existWishlist.get().getItems() == null) {
+            return WishlistDTO.fromEntity(wishlistRepository.save(new Wishlist(userId, Collections.singleton(itemId))));
+        }
+        if (existWishlist.get().getItems().stream().anyMatch(item -> item.getId().equals(itemId))) {
+            throw new IllegalArgumentException("An item with this ID is already in the wishlist");
+        }
+        existWishlist.get().getItems().add(new Item(itemId));
+        return WishlistDTO.fromEntity(wishlistRepository.save(existWishlist.get()));
     }
 }
