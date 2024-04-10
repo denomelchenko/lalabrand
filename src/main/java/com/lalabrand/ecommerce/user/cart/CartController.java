f5da53fa9fc2c70f0a844a140fe6181279d02b1a
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-04-10 17:28:38 +0200

    Add itemToWishlist mutation mapping(CartController)

--- src/main/java/com/lalabrand/ecommerce/user/cart/CartController.java
+++ src/main/java/com/lalabrand/ecommerce/user/cart/CartController.java
@@ -3,10 +3,14 @@ package com.lalabrand.ecommerce.user.cart;
 import com.lalabrand.ecommerce.security.UserDetailsImpl;
 import com.lalabrand.ecommerce.utils.CommonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
+
+import java.util.Set;
 
 @Controller
 public class CartController {
@@ -27,5 +31,11 @@ public class CartController {
             throw new UsernameNotFoundException("User not found!");
         }
         return cartService.findCartByUserId(user.getId()).orElse(null);
+    }
+
+    @MutationMapping(name = "itemToWishlist")
+    @PreAuthorize("hasAuthority('USER')")
+    public CartDTO addItemToWishlist(@Argument String itemInfoId, @Argument String sizeId, @Argument String itemId) {
+        return cartService.addItemToCart(itemId, itemInfoId, sizeId, commonUtils.getCurrentUser().getId());
     }
 }
