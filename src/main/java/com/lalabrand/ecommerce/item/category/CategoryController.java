b35fc7b4e4613b91eca709cc779b5cf15a2afc2a
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-05-01 11:36:06 -0700

    Update CartController

--- src/main/java/com/lalabrand/ecommerce/item/category/CategoryController.java
+++ src/main/java/com/lalabrand/ecommerce/item/category/CategoryController.java
@@ -1,6 +1,7 @@
 package com.lalabrand.ecommerce.item.category;
 
 import com.lalabrand.ecommerce.utils.Id;
+import jakarta.validation.Valid;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
