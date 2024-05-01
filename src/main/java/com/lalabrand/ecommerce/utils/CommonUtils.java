87b40f8170261acea52b36b18e9d2da47b842a63
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-05-01 11:33:57 -0700

    Refactor for CommonUtils

--- src/main/java/com/lalabrand/ecommerce/utils/CommonUtils.java
+++ src/main/java/com/lalabrand/ecommerce/utils/CommonUtils.java
@@ -1,9 +1,13 @@
 package com.lalabrand.ecommerce.utils;
 
 import com.lalabrand.ecommerce.security.UserDetailsImpl;
+import lombok.SneakyThrows;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import java.nio.file.AccessDeniedException;
+import java.util.Arrays;
 import java.util.Collection;
 
 @Component
@@ -12,7 +16,11 @@ public class CommonUtils {
         return ids.stream().anyMatch(id -> id == null || id.isEmpty() || id.contains(" ") || id.length() > 36);
     }
 
-    public static UserDetailsImpl getCurrentUser() {
-        return (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    @SneakyThrows
+    public static String getCurrentUserId() {
+        if (SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof String) {
+            throw new AccessDeniedException("User is not authenticated");
+        }
+        return ((UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId();
     }
 }
