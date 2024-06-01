ca45352d35c53a06a09f21e2b9a304ab40f88d72
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-06-01 11:02:29 +0200

    Implement sizeToItemInfo mutation mapping

--- src/main/java/com/lalabrand/ecommerce/item/item_info/ItemInfoController.java
+++ src/main/java/com/lalabrand/ecommerce/item/item_info/ItemInfoController.java
@@ -1,5 +1,7 @@
 package com.lalabrand.ecommerce.item.item_info;
 
+import com.lalabrand.ecommerce.utils.CommonResponse;
+import com.lalabrand.ecommerce.utils.Id;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -11,6 +13,22 @@ public class ItemInfoController {
 
     public ItemInfoController(ItemInfoService itemInfoService) {
         this.itemInfoService = itemInfoService;
+    }
+
+    @MutationMapping(name = "sizeToItemInfo")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public CommonResponse addSizeToItemInfo(@Argument @Id String itemInfoId, @Argument @Id String sizeId) {
+        if (itemInfoService.addSizeToItemInfo(itemInfoId, sizeId)) {
+            return CommonResponse.builder()
+                    .success(true)
+                    .message("Size added to item info successfully")
+                    .build();
+        } else {
+            return CommonResponse.builder()
+                    .success(false)
+                    .message("Size don`t added to item info")
+                    .build();
+        }
     }
 
     @MutationMapping(name = "itemInfo")
