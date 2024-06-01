c2832bfbf24fed166ef5207322aad5c4adaf0175
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-06-01 11:01:49 +0200

    Implement findAll sizes method(SizeService)

--- src/main/java/com/lalabrand/ecommerce/item/size/SizeService.java
+++ src/main/java/com/lalabrand/ecommerce/item/size/SizeService.java
@@ -2,6 +2,9 @@ package com.lalabrand.ecommerce.item.size;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 public class SizeService {
     private final SizeRepository sizeRepository;
@@ -12,5 +15,17 @@ public class SizeService {
 
     public SizeDTO save(SizeInput sizeInput) {
         return SizeDTO.fromEntity(sizeRepository.save(sizeInput.toEntity()));
+    }
+
+    public Set<SizeDTO> findAll() {
+        return sizeRepository.findAll()
+                .stream()
+                .map(SizeDTO::fromEntity)
+                .collect(Collectors.toSet());
+    }
+
+    public Size findSizeById(String sizeId) {
+        return sizeRepository.findById(sizeId)
+                .orElseThrow(() -> new IllegalArgumentException("Size with this id does not exist"));
     }
 }
