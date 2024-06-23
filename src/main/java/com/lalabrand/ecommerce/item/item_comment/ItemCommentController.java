c219363995e163e65caa0c0a3e0b115a79f5552d
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-06-23 13:52:21 +0200

    Implement ItemCommentController

--- src/main/java/com/lalabrand/ecommerce/item/item_comment/ItemCommentController.java
+++ src/main/java/com/lalabrand/ecommerce/item/item_comment/ItemCommentController.java
@@ -1,7 +1,20 @@
 package com.lalabrand.ecommerce.item.item_comment;
 
+import com.lalabrand.ecommerce.utils.CommonResponse;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ItemCommentController {
+    private final ItemCommentService itemCommentService;
+
+    public ItemCommentController(ItemCommentService itemCommentService) {
+        this.itemCommentService = itemCommentService;
+    }
+
+    @MutationMapping(name = "itemComment")
+    public CommonResponse createItemComment(@Argument(name = "itemCommentInput") ItemCommentInput itemCommentInput) {
+        return itemCommentService.createItemComment(itemCommentInput);
+    }
 }
