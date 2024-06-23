f698fe47ffe6e92e1d94a57dd52a78d0dd8915b1
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-06-23 13:52:54 +0200

    Implement ItemCommentRepository

--- src/main/java/com/lalabrand/ecommerce/item/item_comment/ItemCommentRepository.java
+++ src/main/java/com/lalabrand/ecommerce/item/item_comment/ItemCommentRepository.java
@@ -1,4 +1,8 @@
 package com.lalabrand.ecommerce.item.item_comment;
 
-public interface ItemCommentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemCommentRepository extends JpaRepository<ItemComment, String> {
 }
