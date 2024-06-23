ff0b3ef3dfd79dd51dfab71a2cc1b414dd1fc075
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-06-23 13:52:35 +0200

    Imeplement ItemCommentService

--- src/main/java/com/lalabrand/ecommerce/item/item_comment/ItemCommentService.java
+++ src/main/java/com/lalabrand/ecommerce/item/item_comment/ItemCommentService.java
@@ -1,4 +1,11 @@
 package com.lalabrand.ecommerce.item.item_comment;
 
+import com.lalabrand.ecommerce.utils.CommonResponse;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ItemCommentService {
+    public CommonResponse createItemComment(ItemCommentInput itemCommentInput) {
+        return null;
+    }
 }
