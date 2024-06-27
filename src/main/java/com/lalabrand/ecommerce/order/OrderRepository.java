a0e94cbee114948f376d8783cd0dc6d916120d6f
Nazz <nazz.panasiuk@gmail.com>
2024-06-27 11:07:03 +0300

    Update OrderRepository.java

--- src/main/java/com/lalabrand/ecommerce/order/OrderRepository.java
+++ src/main/java/com/lalabrand/ecommerce/order/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.lalabrand.ecommerce.order;
 
+import com.lalabrand.ecommerce.order.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -14,5 +15,6 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query("SELECT o FROM Order o WHERE o.user.id = :userId ORDER BY o.createdAt DESC")
     List<Order> findAllByUserIdOrderByCreatedAtDesc(String userId);
 
-    Optional<Order> findById(String orderId);
+    @Query("SELECT o FROM Order o WHERE o.status = :status ORDER BY o.createdAt ASC")
+    List<Order> findAllByStatusOrderByCreatedAtAsc(Status status);
 }
