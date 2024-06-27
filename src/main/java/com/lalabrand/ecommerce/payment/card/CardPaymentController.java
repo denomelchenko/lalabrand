09cff83b4f788d454c7f104e4c73ad4a75642d06
Nazz <nazz.panasiuk@gmail.com>
2024-06-27 11:07:43 +0300

    Update CardPaymentController.java

--- src/main/java/com/lalabrand/ecommerce/payment/card/CardPaymentController.java
+++ src/main/java/com/lalabrand/ecommerce/payment/card/CardPaymentController.java
@@ -3,12 +3,15 @@ package com.lalabrand.ecommerce.payment.card;
 import com.lalabrand.ecommerce.order.OrderService;
 import com.lalabrand.ecommerce.order.enums.Currency;
 import com.lalabrand.ecommerce.order.shipping.shipping_info.ShippingInfoRequest;
+import com.lalabrand.ecommerce.payment.paypal.PaypalController;
 import com.lalabrand.ecommerce.utils.CommonUtils;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
 import com.stripe.param.PaymentIntentCreateParams;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.graphql.data.method.annotation.Argument;
@@ -19,6 +22,8 @@ import java.math.BigDecimal;
 
 @Controller
 public class CardPaymentController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CardPaymentController.class);
 
     private final OrderService orderService;
 
@@ -38,7 +43,12 @@ public class CardPaymentController {
     @MutationMapping(name = "createPaymentCard")
     public CardPaymentResponse createPaymentCard(@Argument("currency") Currency currency,
                                                  @Argument("shippingInfo") ShippingInfoRequest shippingInfoRequest) throws StripeException {
-        BigDecimal totalCost = orderService.calculateTotal(CommonUtils.getCurrentUserId());
+        String userId = CommonUtils.getCurrentUserId();
+        logger.info("User {} is attempting to create a payment card with currency {} and shipping info {}", userId, currency, shippingInfoRequest);
+
+        BigDecimal totalCost = orderService.calculateTotal(userId);
+        logger.info("Calculated total cost for user {}: {}", userId, totalCost);
+
         PaymentIntentCreateParams params =
                 PaymentIntentCreateParams.builder()
                         .setAmount(Long.valueOf(String.valueOf(totalCost.multiply(BigDecimal.valueOf(100)))))
@@ -51,14 +61,18 @@ public class CardPaymentController {
                         )
                         .build();
 
-        PaymentIntent paymentIntent = PaymentIntent.create(params);
+        logger.debug("PaymentIntentCreateParams: {}", params);
 
-        orderService.placeOrder(CommonUtils.getCurrentUserId(), shippingInfoRequest, currency);
+        PaymentIntent paymentIntent = PaymentIntent.create(params);
+        logger.info("Created PaymentIntent for user {}: {}", userId, paymentIntent.getId());
+
+        orderService.placeOrder(userId, shippingInfoRequest, currency);
+        logger.info("Placed order for user {}", userId);
 
         return CardPaymentResponse.builder()
                 .success(true)
                 .message("Success to create payment")
-                .clientSecret(paymentIntent.getClientSecret()).
-                build();
+                .clientSecret(paymentIntent.getClientSecret())
+                .build();
     }
 }
