692f589631306c84449da24e1e920484ff6214e4
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-07-01 21:41:13 +0200

    Implement Material Service

---
+++ src/main/java/com/lalabrand/ecommerce/item/material/MaterialService.java
@@ -0,0 +1,10 @@
+package com.lalabrand.ecommerce.item.material;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MaterialService {
+    public MaterialDTO create(String name) {
+        return null;
+    }
+}
