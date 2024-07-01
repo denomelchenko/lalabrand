740f6817b601dea6f4995b24ef9149e96efbdf62
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-07-01 21:40:24 +0200

    Implement Material

--- src/main/java/com/lalabrand/ecommerce/item/material/Material.java
+++ src/main/java/com/lalabrand/ecommerce/item/material/Material.java
@@ -1,7 +1,23 @@
-package com.lalabrand.ecommerce;
+package com.lalabrand.ecommerce.item.material;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
+@Table(name = "materials", schema = "lalabrand")
 public class Material {
-  }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Size(max = 30)
+    @NotNull
+    @Column(name = "name", nullable = false, length = 30)
+    private String name;
+}
