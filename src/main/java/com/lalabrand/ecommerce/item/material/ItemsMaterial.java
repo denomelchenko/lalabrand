bce56b03de908e548a07b9a3948387429dd0080c
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-07-01 21:39:59 +0200

    Implement ItemMaterial

--- src/main/java/com/lalabrand/ecommerce/item/material/ItemsMaterial.java
+++ src/main/java/com/lalabrand/ecommerce/item/material/ItemsMaterial.java
@@ -1,7 +1,29 @@
-package com.lalabrand.ecommerce;
+package com.lalabrand.ecommerce.item.material;
 
-import jakarta.persistence.Entity;
+import com.lalabrand.ecommerce.item.Item;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
+@Table(name = "items_materials", schema = "lalabrand")
 public class ItemsMaterial {
-  }
+    @EmbeddedId
+    private ItemsMaterialId id;
+
+    @MapsId("item")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "item", nullable = false)
+    private Item item;
+
+    @MapsId("materialId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "material_id", nullable = false)
+    private Material material;
+
+    @Column(name = "percentage")
+    private Integer percentage;
+
+}
