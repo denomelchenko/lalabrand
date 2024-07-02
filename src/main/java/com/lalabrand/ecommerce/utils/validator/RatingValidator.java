20445c9451b0f1396fda14b5c689792e27cbba0d
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-07-02 16:39:51 +0200

    Update rating validator

--- src/main/java/com/lalabrand/ecommerce/utils/validator/RatingValidator.java
+++ src/main/java/com/lalabrand/ecommerce/utils/validator/RatingValidator.java
@@ -4,18 +4,15 @@ import com.lalabrand.ecommerce.utils.annotation.Rating;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-import java.math.BigDecimal;
-
-public class RatingValidator implements ConstraintValidator<Rating, BigDecimal> {
+public class RatingValidator implements ConstraintValidator<Rating, Float> {
 
     @Override
     public void initialize(Rating constraintAnnotation) {
     }
 
     @Override
-    public boolean isValid(BigDecimal rating, ConstraintValidatorContext context) {
-        float floatValue = rating.floatValue();
-        return !(floatValue < 1) && !(floatValue > 5)
-                && !rating.toString().matches("^[0-9]+(\\.[0-9])?$");
+    public boolean isValid(Float rating, ConstraintValidatorContext context) {
+        return rating >= 0 && rating <= 5
+                && rating.toString().matches("^[0-5]+(\\.[0-9])?$");
     }
 }
