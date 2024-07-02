12f2f1ed434abf346ee2bca0ad8df4e416fbf79d
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-07-02 16:38:13 +0200

    Update itemCommentController

--- src/main/java/com/lalabrand/ecommerce/item/item_comment/ItemCommentController.java
+++ src/main/java/com/lalabrand/ecommerce/item/item_comment/ItemCommentController.java
@@ -1,8 +1,8 @@
 package com.lalabrand.ecommerce.item.item_comment;
 
 import com.lalabrand.ecommerce.exception.AccessDeniedException;
-import com.lalabrand.ecommerce.utils.CommonResponse;
 import com.lalabrand.ecommerce.utils.CommonUtils;
+import jakarta.validation.Valid;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,9 +18,9 @@ public class ItemCommentController {
 
     @MutationMapping(name = "itemComment")
     @PreAuthorize("hasAuthority('USER')")
-    public ItemCommentDTO createItemComment(@Argument(name = "itemCommentInput") ItemCommentInput itemCommentInput) {
+    public ItemCommentDTO createItemComment(@Argument(name = "itemCommentInput") @Valid ItemCommentInput itemCommentInput) {
         if (CommonUtils.getCurrentUserId().equals(itemCommentInput.getUserId()))
-        return itemCommentService.createItemComment(itemCommentInput);
+            return itemCommentService.createItemComment(itemCommentInput);
         else throw new AccessDeniedException("Access denied");
     }
 }
