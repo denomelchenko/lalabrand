6269acc5344153abeed142aa0ac6315b82e4fd93
Nazz <nazz.panasiuk@gmail.com>
2024-07-09 20:42:37 +0300

    Update FilterRequest.java

--- src/main/java/com/lalabrand/ecommerce/item/filters/FilterRequest.java
+++ src/main/java/com/lalabrand/ecommerce/item/filters/FilterRequest.java
@@ -1,5 +1,6 @@
 package com.lalabrand.ecommerce.item.filters;
 
+import com.lalabrand.ecommerce.item.enums.ColorEnum;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,5 +12,5 @@ public class FilterRequest {
     String categoryId;
     String sizeId;
     String typeOfPriceSort;
-    String color;
+    ColorEnum color;
 }
