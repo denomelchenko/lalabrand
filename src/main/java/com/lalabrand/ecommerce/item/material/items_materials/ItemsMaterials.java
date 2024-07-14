2e03e7a18eddd7210a78ddaa89d9c246b7349193
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-07-14 11:00:01 +0200

    Implement ItemsMaterials

--- src/main/java/com/lalabrand/ecommerce/item/material/items_materials/ItemsMaterials.java
+++ src/main/java/com/lalabrand/ecommerce/item/material/items_materials/ItemsMaterials.java
@@ -1,29 +1,46 @@
-package com.lalabrand.ecommerce.item.material;
+package com.lalabrand.ecommerce.item.material.items_materials;
 
 import com.lalabrand.ecommerce.item.Item;
+import com.lalabrand.ecommerce.item.material.Material;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Builder
 @Table(name = "items_materials", schema = "lalabrand")
-public class ItemsMaterial {
+public class ItemsMaterials {
     @EmbeddedId
-    private ItemsMaterialId id;
+    private ItemsMaterialsId id;
 
     @MapsId("item")
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "item", nullable = false)
+    @JoinColumn(name = "item", nullable = false, insertable = false, updatable = false)
     private Item item;
+
+    @Column(name = "item_id")
+    private String itemId;
 
     @MapsId("materialId")
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "material_id", nullable = false)
+    @JoinColumn(name = "material_id", nullable = false, insertable = false, updatable = false)
     private Material material;
+
+    @Column(name = "material_name")
+    private String materialName;
 
     @Column(name = "percentage")
     private Integer percentage;
 
+
+    public static ItemsMaterials fromItemsMaterialsInput(ItemsMaterialsInput itemsMaterialsInput) {
+        return ItemsMaterials.builder()
+                .itemId(itemsMaterialsInput.getItemId())
+                .materialName(itemsMaterialsInput.getMaterialName())
+                .percentage(itemsMaterialsInput.getPercentage())
+                .build();
+    }
 }
