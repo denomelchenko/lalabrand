8560d818802dbf10d28ae7fe21d7a4f1cde28215
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-07-14 10:57:00 +0200

    Update MaterialService

--- src/main/java/com/lalabrand/ecommerce/item/material/MaterialService.java
+++ src/main/java/com/lalabrand/ecommerce/item/material/MaterialService.java
@@ -2,9 +2,26 @@ package com.lalabrand.ecommerce.item.material;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 public class MaterialService {
+
+    private final MaterialRepository materialRepository;
+
+    public MaterialService(MaterialRepository materialRepository) {
+        this.materialRepository = materialRepository;
+    }
+
     public MaterialDTO create(String name) {
-        return null;
+        return MaterialDTO.fromEntity(materialRepository.save(Material.builder()
+                .name(name)
+                .build()
+        ));
+    }
+
+    public Set<MaterialDTO> findAll() {
+        return materialRepository.findAll().stream().map(MaterialDTO::fromEntity).collect(Collectors.toSet());
     }
 }
