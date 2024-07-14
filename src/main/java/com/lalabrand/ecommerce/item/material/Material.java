b1fc0db3592f8830f0c01c00de1367d2a2499811
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-07-14 10:57:43 +0200

    Update Material entity

--- src/main/java/com/lalabrand/ecommerce/item/material/Material.java
+++ src/main/java/com/lalabrand/ecommerce/item/material/Material.java
@@ -3,21 +3,17 @@ package com.lalabrand.ecommerce.item.material;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "materials", schema = "lalabrand")
 public class Material {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Integer id;
-
-    @Size(max = 30)
-    @NotNull
     @Column(name = "name", nullable = false, length = 30)
     private String name;
 }
