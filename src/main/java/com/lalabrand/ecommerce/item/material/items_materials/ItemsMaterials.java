08ad243b1772c61e7223122b1c00b303a6e71b84
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-07-15 13:09:15 +0200

    Update ItemsMaterials

--- src/main/java/com/lalabrand/ecommerce/item/material/items_materials/ItemsMaterials.java
+++ src/main/java/com/lalabrand/ecommerce/item/material/items_materials/ItemsMaterials.java
@@ -3,34 +3,28 @@ package com.lalabrand.ecommerce.item.material.items_materials;
 import com.lalabrand.ecommerce.item.Item;
 import com.lalabrand.ecommerce.item.material.Material;
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Entity
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "items_materials", schema = "lalabrand")
 public class ItemsMaterials {
     @EmbeddedId
     private ItemsMaterialsId id;
 
-    @MapsId("item")
+    @MapsId("item_id")
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "item", nullable = false, insertable = false, updatable = false)
+    @JoinColumn(name = "item_id", nullable = false, insertable = false, updatable = false)
     private Item item;
 
-    @Column(name = "item_id")
-    private String itemId;
-
-    @MapsId("materialId")
+    @MapsId("material_name")
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "material_id", nullable = false, insertable = false, updatable = false)
+    @JoinColumn(name = "material_name", nullable = false, insertable = false, updatable = false)
     private Material material;
-
-    @Column(name = "material_name")
-    private String materialName;
 
     @Column(name = "percentage")
     private Integer percentage;
@@ -38,8 +32,12 @@ public class ItemsMaterials {
 
     public static ItemsMaterials fromItemsMaterialsInput(ItemsMaterialsInput itemsMaterialsInput) {
         return ItemsMaterials.builder()
-                .itemId(itemsMaterialsInput.getItemId())
-                .materialName(itemsMaterialsInput.getMaterialName())
+                .id(ItemsMaterialsId.builder()
+                        .material_name(itemsMaterialsInput.getMaterialName())
+                        .item_id(itemsMaterialsInput.getItemId())
+                        .build())
+                .item(Item.builder().id(itemsMaterialsInput.getItemId()).build())
+                .material(Material.builder().name(itemsMaterialsInput.getMaterialName()).build())
                 .percentage(itemsMaterialsInput.getPercentage())
                 .build();
     }
