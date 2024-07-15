cf98dcc2c86524abbcf0c5d3d32d33e8d24dc831
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-07-15 13:17:59 +0200

    Update V14_migration

--- src/main/resources/db/migration/V14__materials.sql
+++ src/main/resources/db/migration/V14__materials.sql
@@ -6,10 +6,10 @@ CREATE TABLE `materials`
 
 CREATE TABLE `items_materials`
 (
-    `item`             varchar(36) NOT NULL,
+    `item_id`             varchar(36) NOT NULL,
     `material_name` varchar(30) NOT NULL,
     `percentage`       INT CHECK (percentage > 0 AND percentage <= 100),
-    PRIMARY KEY (`item`, `material_name`),
-    FOREIGN KEY (`item`) REFERENCES `item` (`id`),
+    PRIMARY KEY (`item_id`, `material_name`),
+    FOREIGN KEY (`item_id`) REFERENCES `item` (`id`),
     FOREIGN KEY (`material_name`) REFERENCES `materials` (`name`)
 );
