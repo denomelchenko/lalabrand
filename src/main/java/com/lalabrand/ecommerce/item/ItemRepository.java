df1e6754f059c7add9b116beee973bf3e6b00394
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-07-16 14:36:01 +0200

    Update ItemRepository

--- src/main/java/com/lalabrand/ecommerce/item/ItemRepository.java
+++ src/main/java/com/lalabrand/ecommerce/item/ItemRepository.java
@@ -1,5 +1,6 @@
 package com.lalabrand.ecommerce.item;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,11 +12,11 @@ import java.util.Set;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, String> {
-    List<Item> findItemsByCategoryId(String categoryId, Pageable pageable);
+    Page<Item> findItemsByCategoryId(String categoryId, Pageable pageable);
 
-    List<Item> findItemsByOrderBySoldCountDesc(Pageable pageable);
+    Page<Item> findItemsByOrderBySoldCountDesc(Pageable pageable);
 
-    List<Item> findByTitleContainingIgnoreCase(String title, Pageable pageable);
+    Page<Item> findByTitleContainingIgnoreCase(String title, Pageable pageable);
 
     @Query("select o from Item o where o.id in :itemsIds")
     Set<Item> findAllByIds(Collection<String> itemsIds);
