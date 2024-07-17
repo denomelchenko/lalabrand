f70a0eff949c2a08071c46e050673896038e3b6d
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-07-17 21:14:31 +0200

    Update ItemCommentInput

--- src/main/java/com/lalabrand/ecommerce/item/look/LookController.java
+++ src/main/java/com/lalabrand/ecommerce/item/look/LookController.java
@@ -1,6 +1,5 @@
 package com.lalabrand.ecommerce.item.look;
 
-import org.hibernate.validator.constraints.UUID;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
