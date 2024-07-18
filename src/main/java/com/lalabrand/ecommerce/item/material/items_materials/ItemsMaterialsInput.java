1d89b8c9e40a3c453771c100bfd5c66c6f86606e
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-07-18 13:22:02 +0200

    fixes

--- src/main/java/com/lalabrand/ecommerce/item/material/items_materials/ItemsMaterialsInput.java
+++ src/main/java/com/lalabrand/ecommerce/item/material/items_materials/ItemsMaterialsInput.java
@@ -1,15 +1,15 @@
 package com.lalabrand.ecommerce.item.material.items_materials;
 
-import com.lalabrand.ecommerce.utils.annotation.Id;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Value;
+import org.hibernate.validator.constraints.UUID;
 
 @Value
 public class ItemsMaterialsInput {
-    @Id
+    @UUID(message = "ItemId is not valid")
     String itemId;
 
     @NotNull
